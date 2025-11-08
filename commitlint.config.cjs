3381f1f168887b0bc94bd0d6a73c50c66c8c738e
Manu <manueljesusrejonsantana@gmail.com>
2025-11-07 21:25:34 -0600

    First Commit

---
+++ commitlint.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    // Opcional: relajar reglas para emojis/mayúsculas en el subject
+    // rules: { 'subject-case': [0] }
+};
